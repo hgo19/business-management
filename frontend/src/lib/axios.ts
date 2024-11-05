0e289875d4864fc04be84a88ce91c8a652ead7f8
Hugo Leonardo <huleonardo19@gmail.com>
2024-11-05 19:13:27 -0300

    feat: create axios config file

---
+++ frontend/src/lib/axios.ts
@@ -0,0 +1,47 @@
+import axios, { AxiosInstance, InternalAxiosRequestConfig, AxiosResponse } from 'axios';
+import Cookies from 'js-cookie';
+
+interface RefreshTokenResponse {
+  access_token: string;
+}
+
+const api: AxiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+  const token = Cookies.get('access_token');
+  if (token) {
+    config.headers.set('Authorization', `Bearer ${token}`);
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (response: AxiosResponse) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const refreshToken = Cookies.get('refresh_token');
+        const { data } = await api.post<RefreshTokenResponse>('/auth/refresh_token', { refresh_token: refreshToken });
+        const { access_token } = data;
+        Cookies.set('access_token', access_token);
+        originalRequest.headers.set('Authorization', `Bearer ${access_token}`);
+        return api(originalRequest);
+      } catch (refreshError) {
+        Cookies.remove('access_token');
+        Cookies.remove('refresh_token');
+        window.location.href = '/login';
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
