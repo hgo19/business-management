b265e18e188045b40f74c7e4d90d98e57385ba0d
Hugo Leonardo <huleonardo19@gmail.com>
2024-11-04 22:33:31 -0300

    feat: creates user model

---
+++ backend/src/models/user.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel, Field, EmailStr, validator
+from enum import Enum
+import re
+
+
+class Roles(str, Enum):
+    admin = "admin"
+    superadmin = "super_admin"
+    operator = "operator"
+
+
+class UserBase(BaseModel):
+    name: str = Field(..., title="Name", description="The name of the user")
+    email: EmailStr = Field(..., title="Email", description="The email of the user")
+    role: Roles = Field(..., title="Role", description="The role of the user")
+    company_id: Optional[str] = Field(
+        None,
+        title="Company ID",
+        description="The ID of the company associated with the user",
+    )
+
+
+class UserCreate(UserBase):
+    password: str = Field(
+        ...,
+        min_length=6,
+        description="Password with at least 1 uppercase letter, 1 number, and min 6 characters",
+    )
+
+    @validator("password")
+    def validate_password(cls, v):
+        if not re.search(r"^(?=.*[A-Z])(?=.*\d).{6,}$", v):
+            raise ValueError(
+                "Password must contain at least 1 uppercase letter, 1 number, and be at least 6 characters long"
+            )
+        return v
+
+
+class UserRead(UserCreate):
+    id: str
+    refresh_token: Optional[str]
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        from_attributes = True
+
+
+class UserLogin(BaseModel):
+    email: str
+    password: str
