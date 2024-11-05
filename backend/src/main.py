a084a26aeff7ce3d8881f326bd7b62b6803ef401
Hugo Leonardo <huleonardo19@gmail.com>
2024-11-05 19:11:34 -0300

    feat: add static allow origins for now

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,16 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.database.connection import init_db, dispose_engine
-from src.routes import (
-    auth,
-    users
-)
+from src.routes import auth, users
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
