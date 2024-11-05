1f54c94ec08c3d332ce4b5ddf8c8c1dccb51ac1f
Hugo Leonardo <huleonardo19@gmail.com>
2024-11-04 22:42:37 -0300

    feat: creates token model

---
+++ backend/src/models/token.py
@@ -0,0 +1,16 @@
+from typing import Optional
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class Token(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+
+
+class TokenData(BaseModel):
+    id: str
+    name: str
+    email: str
+    exp: Optional[datetime] = None
