18f79856d17d77b394ca307e2a9e1448835862f8
Hugo Leonardo <huleonardo19@gmail.com>
2024-11-06 18:32:39 -0300

    feat: add joinedload on the cruds repositories

--- backend/src/repository/company_crud.py
+++ backend/src/repository/company_crud.py
@@ -1,70 +1,91 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import NoResultFound
 from sqlalchemy.future import select
-from src.models.company import CompanyCreate, CompanyRead, CompanyUpdate
+from src.models.company import CompanyCreate, CompanyResponse, CompanyUpdate
 from src.database.schemas import Company
-
+from sqlalchemy.orm import joinedload
 
 class CompanyCrudRepository:
     def __init__(self, database: AsyncSession) -> None:
         self.db: AsyncSession = database
 
-    async def create(self, company: CompanyCreate) -> CompanyRead:
+    async def create(self, company: CompanyCreate) -> CompanyResponse:
         company_entity = Company(**company.dict())
         self.db.add(company_entity)
         await self.db.commit()
         await self.db.refresh(company_entity)
-        return CompanyRead.from_orm(company_entity)
+        return CompanyResponse.from_orm(company_entity)
 
-    async def get_by_id(self, company_id: str) -> CompanyRead:
-        result = await self.db.execute(select(Company).filter(Company.id == company_id))
-        company = result.scalars().first()
+    async def get_by_id(self, company_id: str) -> CompanyResponse:
+        result = await self.db.execute(
+            select(Company)
+            .options(
+                joinedload(Company.users),
+                joinedload(Company.admin)
+            )
+            .filter(Company.id == company_id)
+        )
+        company = result.unique().scalars().first()
         if not company:
             raise NoResultFound(f"Company with id {company_id} not found")
-        return CompanyRead.from_orm(company)
+        return CompanyResponse.from_orm(company)
 
-    async def get_by_email(self, contact_email: str) -> CompanyRead | None:
+    async def get_by_email(self, contact_email: str) -> CompanyResponse | None:
         result = await self.db.execute(
-            select(Company).filter(Company.contact_email == contact_email)
+            select(Company)
+            .options(
+                joinedload(Company.users),
+                joinedload(Company.admin)
+            )
+            .filter(Company.contact_email == contact_email)
         )
-        company = result.scalars().first()
+        company = result.unique().scalars().first()
         if not company:
             return None
-        return CompanyRead.from_orm(company)
+        return CompanyResponse.from_orm(company)
 
-    async def get_all(self) -> list[CompanyRead]:
-        result = await self.db.execute(select(Company))
-        companies = result.scalars().all()
-        return [CompanyRead.from_orm(company) for company in companies]
+    async def get_all(self) -> list[CompanyResponse]:
+        result = await self.db.execute(
+            select(Company)
+            .options(
+                joinedload(Company.users),
+                joinedload(Company.admin)
+            )
+        )
+        companies = result.unique().scalars().all()
+        return [CompanyResponse.from_orm(company) for company in companies]
 
-    async def update(
-        self, company_id: str, company_update: CompanyUpdate
-    ) -> CompanyRead:
+    async def update(self, company_id: str, company_update: CompanyUpdate) -> CompanyResponse:
         result = await self.db.execute(select(Company).filter(Company.id == company_id))
         company = result.scalars().first()
         if not company:
             raise NoResultFound(f"Company with id {company_id} not found")
-
         company_data = company_update.dict(exclude_unset=True)
         for key, value in company_data.items():
             setattr(company, key, value)
-
         await self.db.commit()
         await self.db.refresh(company)
-        return CompanyRead.from_orm(company)
+        return CompanyResponse.from_orm(company)
 
-    async def delete(self, company_id: str) -> None:
+    async def delete(self, company_id: str) -> bool:
         result = await self.db.execute(select(Company).filter(Company.id == company_id))
         company = result.scalars().first()
         if not company:
             raise NoResultFound(f"Company with id {company_id} not found")
-
         await self.db.delete(company)
         await self.db.commit()
+        return True
 
-    async def get_by_name(self, name: str) -> CompanyRead | None:
-        result = await self.db.execute(select(Company).filter(Company.name == name))
-        company = result.scalars().first()
+    async def get_by_name(self, name: str) -> CompanyResponse | None:
+        result = await self.db.execute(
+            select(Company)
+            .options(
+                joinedload(Company.users),
+                joinedload(Company.admin)
+            )
+            .filter(Company.name == name)
+        )
+        company = result.unique().scalars().first()
         if not company:
             return None
-        return CompanyRead.from_orm(company)
+        return CompanyResponse.from_orm(company)
--- backend/src/repository/user_crud.py
+++ backend/src/repository/user_crud.py
@@ -1,72 +1,104 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import NoResultFound
 from sqlalchemy.future import select
-from ..models.user import UserCreate, UserRead
-from ..database.schemas import User
-
+from sqlalchemy.orm import joinedload
+from ..models.user import UserCreate, UserResponse, UserUpdate, UserRead
+from ..database.schemas import User, Roles
+from sqlalchemy.dialects.postgresql import dialect
 
 class UserCrudRepository:
     def __init__(self, database: AsyncSession) -> None:
         self.db: AsyncSession = database
 
-    async def create(self, user: UserCreate) -> UserRead:
+    async def create(self, user: UserCreate) -> UserResponse:
         user_entity = User(**user.dict())
         self.db.add(user_entity)
         await self.db.commit()
         await self.db.refresh(user_entity)
-        return UserRead.from_orm(user_entity)
+        return UserResponse.from_orm(user_entity)
 
-    async def get_by_id(self, user_id: str) -> UserRead:
-        result = await self.db.execute(select(User).filter(User.id == user_id))
-        user = result.scalars().first()
+    async def get_by_id(self, user_id: str) -> UserResponse:
+        result = await self.db.execute(
+            select(User)
+            .options(
+                joinedload(User.company),
+                joinedload(User.administered_companies)
+            )
+            .filter(User.id == user_id)
+        )
+        user = result.unique().scalars().first()
         if not user:
             raise NoResultFound(f"User with id {user_id} not found")
-        return UserRead.from_orm(user)
+        return UserResponse.from_orm(user)
 
     async def get_by_email(self, email: str) -> UserRead | None:
-        result = await self.db.execute(select(User).filter(User.email == email))
-        user = result.scalars().first()
+        result = await self.db.execute(
+            select(User)
+            .options(
+                joinedload(User.company),
+                joinedload(User.administered_companies)
+            )
+            .filter(User.email == email)
+        )
+        user = result.unique().scalars().first()
         if not user:
             return None
         return UserRead.from_orm(user)
 
-    async def get_all(self) -> list[UserRead]:
-        result = await self.db.execute(select(User))
-        users = result.scalars().all()
-        return [UserRead.from_orm(user) for user in users]
+    async def get_all(self) -> list[UserResponse]:
+        result = await self.db.execute(
+            select(User)
+            .options(
+                joinedload(User.company),
+                joinedload(User.administered_companies)
+            )
+        )
 
-    async def update(self, user_id: str, user_update: UserCreate) -> UserRead:
+
+        users = result.unique().scalars().all()
+        return [UserResponse.from_orm(user) for user in users]
+
+    async def update(self, user_id: str, user_update: UserUpdate) -> UserResponse:
         result = await self.db.execute(select(User).filter(User.id == user_id))
         user = result.scalars().first()
         if not user:
             raise NoResultFound(f"User with id {user_id} not found")
-
         user_data = user_update.dict(exclude_unset=True)
         for key, value in user_data.items():
             setattr(user, key, value)
-
         await self.db.commit()
         await self.db.refresh(user)
-        return UserRead.from_orm(user)
+        return UserResponse.from_orm(user)
 
     async def delete(self, user_id: str) -> bool:
         result = await self.db.execute(select(User).filter(User.id == user_id))
         user = result.scalars().first()
         if not user:
             raise NoResultFound(f"User with id {user_id} not found")
-
         await self.db.delete(user)
         await self.db.commit()
+        return True
 
-    async def get_by_company_id(self, company_id: str) -> list[UserRead]:
+    async def get_by_company_id(self, company_id: str) -> list[UserResponse]:
         result = await self.db.execute(
-            select(User).filter(User.company_id == company_id)
+            select(User)
+            .options(
+                joinedload(User.company),
+                joinedload(User.administered_companies)
+            )
+            .filter(User.company_id == company_id)
         )
-        users = result.scalars().all()
-        return [UserRead.from_orm(user) for user in users]
-    
+        users = result.unique().scalars().all()
+        return [UserResponse.from_orm(user) for user in users]
 
-    async def get_all_admins(self) -> list[UserRead]:
-        result = await self.db.execute(select(User).filter(User.role == "admin"))
-        users = result.scalars().all()
-        return [UserRead.from_orm(user) for user in users]
+    async def get_all_admins(self) -> list[UserResponse]:
+        result = await self.db.execute(
+            select(User)
+            .options(
+                joinedload(User.company),
+                joinedload(User.administered_companies)
+            )
+            .filter(User.role == Roles.admin)
+        )
+        users = result.unique().scalars().all()
+        return [UserResponse.from_orm(user) for user in users]
